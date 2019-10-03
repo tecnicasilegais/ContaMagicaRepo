5b1265140ba6d58265a4186b63d68238c5501278
MarceloHeredia <marcelo.heredia@acad.pucrs.br>
2019-10-03 20:50:49 -0300

    a

---
+++ src/test/java/com/taMagica/ContaMagicaTest.java
@@ -0,0 +1,17 @@
+package com.taMagica;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.*;
+import org.junit.jupiter.api.Test;
+
+public class ContaMagicaTest {
+    private static final int SILVER = 0;
+    private static final int GOLD = 1;
+    private static final int PLATINUM = 2;
+    
+    @Test
+    public void test1(){
+        assertTrue(true);
+    }
+}
